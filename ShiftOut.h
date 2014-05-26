89a8df6bb667238f693bef6c05027bcc13ce3068
ollie8 <ollie8@localhost>
2014-05-26 14:57:22 +0000

    Library for controlling a shift resgister

---
+++ ShiftOut.h
@@ -0,0 +1,40 @@
+#ifndef SHIFTOUT_H
+#define SHIFTOUT_H
+
+#include <mbed.h>
+
+class ShiftOut {
+
+    public :
+    
+        ShiftOut(PinName clk, PinName data, PinName latch, int8_t registerCount = 8) {
+            clkout = new DigitalOut(clk);
+            dataout = new DigitalOut(data);
+            latchout = new DigitalOut(latch);
+            this->registerCount = registerCount;
+        }
+        
+        ~ShiftOut() {
+            delete clkout;
+            delete dataout;
+            delete latchout;
+        }
+        
+        void write(int8_t data) {
+            *latchout = 0;
+            for(int i = registerCount - 1; i >=  0; i--){
+                *clkout = 0;
+                *dataout = (data & (1 << i)) != 0;
+                *clkout = 1;
+            }
+            *latchout = 1;
+        }
+         
+    private :
+        DigitalOut *clkout;
+        DigitalOut *dataout;
+        DigitalOut *latchout;
+        int8_t registerCount;     
+};
+
+#endif
